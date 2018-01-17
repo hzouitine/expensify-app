11f7f5cdc4873c191b206817178b03160cfdd0b9
hzouitine <3zouitine@gmail.com>
2018-01-17 18:53:04 +0000

    Import Switch from React-Router. Add PageNotFound path & component.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 console.log("app.js");
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
@@ -23,21 +23,26 @@ const EditExpensePage = () => (
     </div>
 );
 
-const helpPage = () => (
+const HelpPage = () => (
     <div>
     helpPage Componenet
+    </div>
+);
+const NotFoundPage = () => (
+    <div>
+    Error 404 !
     </div>
 );
 
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true}/>
         <Route path="/create" component={AddExpensePage} />        
         <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={helpPage} />
-
-        </div>
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 )
 
