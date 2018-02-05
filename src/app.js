7fbec9a16eb601d589347dabff54e5381742e694
hzouitine <3zouitine@gmail.com>
2018-02-05 21:36:46 +0000

    Organizing redux : add actions,reducers,store and selector folder. import store in app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,17 @@ import {BrowserRouter, Switch, Link,NavLink, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
+const store = configureStore();
+store.dispatch(addExpense({ description : 'Water Bill'}));
+store.dispatch(addExpense({ description : 'Gas Bill'}));
+store.dispatch(setTextFilter("bill"));
+const state = store.getState();
+const visibleExpense = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpense);
 
 ReactDOM.render(<AppRouter />,document.getElementById("appRoot"));
