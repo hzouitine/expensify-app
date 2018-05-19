aea67f3f06e986dc99a8cccbac095a4f03c37295
hzouitine <3zouitine@gmail.com>
2018-05-19 13:37:38 +0000

    Add component ExpenseForm containing a form for creating/editing expense

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 const AddExpensePage = () => (
     <div>
     AddExpensePage Componenet
+    <ExpenseForm />
     </div>
 );
 
---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+export default class ExpenseForm extends React.Component{
+
+    state = {
+        description : '',
+        note : '',
+        amount : ''
+    }
+
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState( () => ({
+            description
+        }));
+        console.log(this.state);
+    }
+
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+        const regex = /^\d*(\.\d{0,2})?$/;
+        if(amount.match(regex))
+        this.setState( () => ({
+            amount
+        }));
+        console.log(this.state);
+    }
+    
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState( () => ({
+            note
+        }));
+        console.log(this.state);
+    }
+    render(){
+        return (
+            <div>
+                <form>
+                    <input 
+                        type="text"
+                        placeholder="Description"
+                        autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange} />
+                    <input 
+                        type="text"
+                        placeholder="Amount"
+                        value={this.state.amount}
+                        onChange={this.onAmountChange} />
+                    <br />
+                    <textarea 
+                        placeholder="Add a note for your expense (optional)"
+                        onChange={this.onNoteChange} ></textarea>
+                    <input type="submit" value="Add Expense" />
+                </form>
+             </div>
+        );
+    }
+}
